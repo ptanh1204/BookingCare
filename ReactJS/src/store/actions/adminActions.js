45cde25233c8e8e9f170a29aab00efd113a64347
Tuan Anh <pewpewsts@gmail.com>
2024-12-13 23:57:26 +0700

    master

--- ReactJS/src/store/actions/adminActions.js
+++ ReactJS/src/store/actions/adminActions.js
@@ -1,13 +1,83 @@
 import actionTypes from './actionTypes';
+import { getAllCodeServiece } from '../../services/userService';
+//Gender
+export const fetchGenderStart = () => {
+    return async (dispatch, getState) => {
+        try {
+            dispatch({ type: actionTypes.FETCH_GENDER_START })
+            let res = await getAllCodeServiece("GENDER");
+            if (res && res.errCode === 0) {
+                dispatch(fetchGenderSucess(res.data))
+            } else {
+                dispatch(fetchGenderFailed());
+            }
+        } catch (e) {
+            dispatch(fetchGenderFailed());
+            console.log('fetchGenderStart error', e)
+        }
+    }
+}
 
-export const fetchGenderStart = () => ({
-    type: actionTypes.FETCH_GENDER_START
-})
 
-export const fetchGenderSucess = () => ({
-    type: actionTypes.FETCH_GENDER_SUCCESS
+
+export const fetchGenderSucess = (genderData) => ({
+    type: actionTypes.FETCH_GENDER_SUCCESS,
+    data: genderData
 })
 
 export const fetchGenderFailed = () => ({
     type: actionTypes.FETCH_GENDER_FAILED
+})
+
+//Position
+
+export const fetchPositionStart = () => {
+    return async (dispatch, getState) => {
+        try {
+            let res = await getAllCodeServiece("POSITION");
+            if (res && res.errCode === 0) {
+                dispatch(fetchPositionSucess(res.data))
+            } else {
+                dispatch(fetchPositionFailed());
+            }
+        } catch (e) {
+            dispatch(fetchPositionFailed());
+            console.log('fetchPositionStart error', e)
+        }
+    }
+}
+export const fetchPositionSucess = (positionData) => ({
+    type: actionTypes.FETCH_POSITION_SUCCESS,
+    data: positionData
+})
+
+export const fetchPositionFailed = () => ({
+    type: actionTypes.FETCH_POSITION_FAILED,
+})
+
+//Role
+
+export const fetchRoleStart = () => {
+    return async (dispatch, getState) => {
+        try {
+            let res = await getAllCodeServiece("ROLE");
+            if (res && res.errCode === 0) {
+                dispatch(fetchRoleSucess(res.data))
+            } else {
+                dispatch(fetchRoleFailed());
+            }
+        } catch (e) {
+            dispatch(fetchRoleFailed());
+            console.log('fetchRoleStart error', e)
+        }
+    }
+}
+
+export const fetchRoleSucess = (roleData) => ({
+    type: actionTypes.FETCH_ROLE_SUCCESS,
+    data: roleData
+})
+
+export const fetchRoleFailed = () => ({
+    type: actionTypes.FETCH_ROLE_FAILED,
 })
---
+++ ReactJS/src/store/reducers/adminReducer.js
@@ -0,0 +1,68 @@
+import actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    isLoadingGender: false,
+    genders: [],
+    roles: [],
+    positions: [],
+    roles: []
+}
+
+const adminReducer = (state = initialState, action) => {
+    switch (action.type) {
+        //gender
+        case actionTypes.FETCH_GENDER_START:
+            let copyState = { ...state };
+            copyState.isLoadingGender = true;
+            return {
+                ...copyState
+
+            }
+        case actionTypes.FETCH_GENDER_SUCCESS:
+
+            state.genders = action.data;
+            state.isLoadingGender = false;
+            return {
+                ...state,
+
+            }
+        case actionTypes.FETCH_GENDER_FAILED:
+            state.isLoadingGender = false;
+            state.genders = [];
+            return {
+                ...state,
+
+            }
+        //position
+        case actionTypes.FETCH_POSITION_SUCCESS:
+            state.positions = action.data;
+            return {
+                ...state,
+
+            }
+        case actionTypes.FETCH_POSITION_FAILED:
+            state.isLoadingPosition = false;
+            state.positions = [];
+            return {
+                ...state,
+
+            }
+        //role
+        case actionTypes.FETCH_ROLE_SUCCESS:
+            state.roles = action.data;
+            return {
+                ...state,
+
+            }
+        case actionTypes.FETCH_ROLE_FAILED:
+            state.roles = [];
+            return {
+                ...state,
+
+            }
+        default:
+            return state;
+    }
+}
+
+export default adminReducer;
